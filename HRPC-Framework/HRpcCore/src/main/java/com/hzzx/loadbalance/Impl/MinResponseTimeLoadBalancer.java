269f74ff1dc3e8ba65265492c75498d670346474
Hzzxuan <hzxuan1225@gmail.com>
2024-06-04 21:07:58 +0800

    新增最小响应时间负载均衡

--- HRPC-Framework/HRpcCore/src/main/java/com/hzzx/loadbalance/Impl/MinResponseTimeLoadBalancer.java
+++ HRPC-Framework/HRpcCore/src/main/java/com/hzzx/loadbalance/Impl/MinResponseTimeLoadBalancer.java
@@ -1,13 +1,16 @@
 package com.hzzx.loadbalance.Impl;
 
+import com.hzzx.HBootstrap;
 import com.hzzx.discovery.Registry;
 import com.hzzx.loadbalance.AbstractLoadBalancer;
 import com.hzzx.loadbalance.LoadBalancer;
 import com.hzzx.loadbalance.Selector;
+import io.netty.channel.Channel;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetSocketAddress;
 import java.util.List;
+import java.util.SortedMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -15,41 +18,32 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @date : 2024/6/4 16:00
  */
 @Slf4j
-public class MinResponseTimeLoadBalancer extends AbstractLoadBalancer {
+public class MinResponseTimeLoadBalancer implements LoadBalancer {
+    //维护的是Map<ServiceName,SortedMap>
+    private Registry registry;
 
-    public MinResponseTimeLoadBalancer(Registry registry) {
-        super(registry);
+
+    //todo:registry全局配置
+    public MinResponseTimeLoadBalancer(Registry registry){
+        this.registry = registry;
     }
-
     @Override
-    protected Selector getSelector(List<InetSocketAddress> serviceList) {
-        return null;
-    }
-
-    private static class RoundRobinSelector implements Selector{
-        private List<InetSocketAddress> serviceList;
-        private AtomicInteger index = new AtomicInteger(0);
-        public RoundRobinSelector(List<InetSocketAddress> serviceList){
-            this.serviceList = serviceList;
+    public InetSocketAddress chooseServiceAddress(String serviceName){
+        SortedMap<Long, Channel> channelSortedMap = HBootstrap.ANSWER_TIME_CHANNEL_CACHE.get(serviceName);
+        if(!channelSortedMap.isEmpty()){
+            log.debug("返回一最小响应结果");
+            return (InetSocketAddress) channelSortedMap.get(channelSortedMap.firstKey()).remoteAddress();
         }
-        @Override
-        public InetSocketAddress getNext() {
+        else {
+            log.debug("无最小响应结果，返回默认");
+            List<InetSocketAddress> serviceList = registry.lookup(serviceName);
             if(serviceList.isEmpty() || serviceList == null){
                 log.error("无可用服务节点");
                 //todo 自定义exception表示
                 throw new RuntimeException();
             }
-            int i = index.get();
-            if(i == serviceList.size()-1){
-                index.set(0);
-            }
-            index.incrementAndGet();
-            return serviceList.get(i);
-        }
-
-        @Override
-        public void renewBalance() {
-
+            return serviceList.get(0);
         }
     }
+
 }
